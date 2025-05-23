fdcc7a5f50515d1cecbaa23e9ccb9bb846fe574e
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-05-23 18:24:22 +0530

    Initial Commit

---
+++ ReplyWise_Backend/src/main/java/com/ansh/ReplyWise_Backend/ReplyWiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ansh.ReplyWise_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplyWiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplyWiseBackendApplication.class, args);
+	}
+
+}
---
+++ ReplyWise_Backend/src/test/java/com/ansh/ReplyWise_Backend/ReplyWiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansh.ReplyWise_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplyWiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
