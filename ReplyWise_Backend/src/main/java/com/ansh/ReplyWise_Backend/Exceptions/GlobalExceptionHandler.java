9c5a8b983d7d0ece70a36e0f041ee1b60622a455
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-05-26 12:50:37 +0530

    Updated GlobalExceptionHandler for general errors

--- ReplyWise_Backend/src/main/java/com/ansh/ReplyWise_Backend/Exceptions/GlobalExceptionHandler.java
+++ ReplyWise_Backend/src/main/java/com/ansh/ReplyWise_Backend/Exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,19 @@
 package com.ansh.ReplyWise_Backend.Exceptions;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler{
 
+    @EventListener(Exception.class)
+    public ErrorResponse generalErrors(Exception e){
+
+        int status = HttpStatus.INTERNAL_SERVER_ERROR.value();
+
+        return new ErrorResponse(status, e.getMessage(), LocalDateTime.now());
+    }
 }
