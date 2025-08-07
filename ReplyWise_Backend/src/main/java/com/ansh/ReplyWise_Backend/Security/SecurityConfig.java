cb9dfe1658560d4655f7fdb96e3ade9e523ec522
Nimish22211 <nimishbandha720s@gmail.com>
2025-08-07 14:21:43 +0530

    updated security routes

--- ReplyWise_Backend/src/main/java/com/ansh/ReplyWise_Backend/Security/SecurityConfig.java
+++ ReplyWise_Backend/src/main/java/com/ansh/ReplyWise_Backend/Security/SecurityConfig.java
@@ -26,8 +26,9 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
-                        .requestMatchers("/health").permitAll()
+                        .requestMatchers("/**").permitAll()
+                        // .requestMatchers("/api/**").permitAll()
+                        // .requestMatchers("/health").permitAll()
                         .anyRequest().authenticated()
                 )
 
